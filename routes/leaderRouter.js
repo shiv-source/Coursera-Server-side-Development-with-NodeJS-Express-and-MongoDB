882dccbe1c3105c1fc2a0eedbfa3debf1ecffd29
shiv-source <shivk7466@gmail.com>
2020-11-10 13:30:11 +0530

    Express Generator REST API

---
+++ routes/leaderRouter.js
@@ -0,0 +1,47 @@
+const express = require('express');
+// const bodyParser = require('body-parser');
+
+const leaderRouter = express.Router();
+
+// leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+.get((req,res,next) => {
+    res.end('Will send all the leaderses to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the leaders: ' + req.body.name + ' with details: ' + req.body.description);
+});
+
+
+leaderRouter.route('/:leaderId')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+.get((req,res,next) =>{
+    return res.end(`Will return a specific leaders with id ${req.params.leaderId}` );
+
+})
+
+.put((req,res,next) =>{
+    return res.end(`Will modify a specific leaders with id ${req.params.leaderId}` );
+
+})
+
+.delete((req,res,next) =>{
+    return res.end(`Will delete a specific leaders with id ${req.params.leaderId}` );
+
+});
+
+
+
+module.exports = leaderRouter;
---
+++ routes/promoRouter.js
@@ -0,0 +1,47 @@
+const express = require('express');
+// const bodyParser = require('body-parser');
+
+const promoRouter = express.Router();
+
+// promoRouter.use(bodyParser.json());
+
+promoRouter.route('/')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+.get((req,res,next) => {
+    res.end('Will send all the promotionses to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the promotions: ' + req.body.name + ' with details: ' + req.body.description);
+});
+
+
+promoRouter.route('/:promoId ')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+.get((req,res,next) =>{
+    return res.end(`Will return a specific promotions with id ${req.params.promoId }` );
+
+})
+
+.put((req,res,next) =>{
+    return res.end(`Will modify a specific promotions with id ${req.params.promoId }` );
+
+})
+
+.delete((req,res,next) =>{
+    return res.end(`Will delete a specific promotions with id ${req.params.promoId }` );
+
+});
+
+
+
+module.exports = promoRouter;
